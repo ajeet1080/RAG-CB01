01e2fa749c5a7acc1b37c0f1d9cbb98419d9419d
Ajeet <08.ajeet@gmail.com>
2024-01-26 18:47:56 +0800

    Updated app

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from models import initialize_db, EMD, emdSchema,  DRUG, drugSchema, LAB, labSch
 import openai
 from sqlalchemy import func
 
-# test
+
+
+import os
+
+# test1
 app = Flask(__name__)
 
 # Initialize the database with the app instance
@@ -18,8 +22,8 @@ app.app_context().push()
 openai.api_type = "azure"
 openai.api_version = "2023-05-15"
 # Your Azure OpenAI resource's endpoint value.
-openai.api_base = "https://singhealth-openai.openai.azure.com/"
-openai.api_key = "8878aae71e9d425ca35e7a2c70d8f9af"
+openai.api_base = "https://shplayground2.openai.azure.com/"
+openai.api_key = "fefc20d1c3ee4046b446c239f96e4fc4"
 
 
 @app.route('/static/<path:path>')
@@ -334,7 +338,7 @@ def get_openai_response():
     user_message = request.json.get('prompt')
     response = openai.ChatCompletion.create(
         # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
-        engine="mhdhmllm1",
+        engine="432",
         messages=[
             {"role": "system",
                 "content": "You are medical Assistant that can generate medical reports based on the given patient's medical data."},
@@ -342,6 +346,132 @@ def get_openai_response():
         ] , temperature=0.3,top_p=1 
     )
     return jsonify(response['choices'][0]['message'])
+
+@app.route('/compare', methods=['POST'])
+def get_comparision_response():
+    user_message = request.json.get('prompt')
+    response = openai.ChatCompletion.create(
+        # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
+        engine="432",
+        messages=[
+            {"role": "system",
+                "content": "Extract the list of entities and their values from the provided Source Data and Medical Report in tabular format with 3 columns named as Entity ,Source Data value, Medical Report Value .\n\nAn Entity is:\n- Relevant: to the main story.\n- Specific: descriptive yet concise (5 words or fewer).\n- Faithful: present in the provided data.\n- Anywhere: Located anywhere in provided data."},
+            {"role": "user", "content": user_message}
+        ] , temperature=0.3,top_p=1 
+    )
+    return jsonify(response['choices'][0]['message'])
+
+@app.route('/format_transcript', methods=['POST'])
+def get_format_transcript():
+    user_message = request.json.get('transcript')
+    response = openai.ChatCompletion.create(
+        # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
+        engine="432",
+        messages=[
+            {"role": "system",
+                "content": "You will be provided with a transcript of a conversation between a doctor and a patient in either of English , Mandarin , Indonesian or Tamil language. You need to re format the transcript in English in a way that it is easy to read and understand.Please ensure to do proper tagging as Doctor , Patient. You can use any format provided in Sample Transcript below. Do not add any additional information to the transcript. \n\nSample Transcript:\nDoctor: Hello, how are you?\nPatient: I am fine, thank you.\nDoctor: What brings you here today?\nPatient: I have a headache.\nDoctor: How long have you had it?\nPatient: For about a week."},
+            {"role": "assistant", "content": user_message}
+        ] , temperature=0.2,top_p=1 
+    )
+    return jsonify(response['choices'][0]['message'])
+
+@app.route('/summarize_transript', methods=['POST'])
+def get_summarize_transcript():
+    user_message = request.json.get('transcript')
+    response = openai.ChatCompletion.create(
+        # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
+        engine="432",
+        messages=[
+            {"role": "system",
+                "content": "You will be provided with a transcript of a conversation between a doctor and a patient in either of English , Mandarin , Indonesian or Tamil language. You need to summarize the transcript in English in a way that it is easy to read and understand. Summary should include Problem, Medical history, Medications, Allergies, Family history, Social history, Physical exam, Assessment, Plan. \n\nSample Summary: \nProblem: \nPatient has a headache for about a week. \n\nMedical history: \nPatient has no medical history. \n\nMedication:\n Patient is not taking any medications. \n\nAllergies:\n Patient has no allergies. Patient has no family history. \n\nFamily history:\n Patient has no social history. \n\nPhysical examination:\n Patient has no physical exam. \n\nAssessment:\n Patient has no assessment. \n\nPlan:\n Patient has no plan."},
+            {"role": "assistant", "content": user_message}
+        ] , temperature=0.2,top_p=1 
+    )
+    return jsonify(response['choices'][0]['message'])
+
+@app.route('/solve-mystery', methods=['POST'])
+def solve_mystery():
+    user_input = request.json['text']
+    initial_context = """You are a database of evidence that will be used to solve the mystery of the missing merlion. Detectives will ask you questions to retrieve the evidence. Use the following guidelines when giving answers:
+
+Answer questions in less than 50 words.
+If asked for the clues or evidence, reply with a random Singapore fun fact.
+If the answer cannot be found, create a random story.
+If the question is irrelevant, reply with a random Singapore fun fact.
+
+Evidence
+
+Merlion Sightings: The merlion was last seen by credible sources at 1:30am on 2 Jan 2024.
+
+Site investigations:
+ -Only nuts and bolts were left scattered around the scene.
+ -The floor was undamaged and there are no signs of the Merlion being forcefully removed.
+ -Skidding marks were seen towards the direction of the river.
+
+Surveillance footage:
+ -A medium-sized box truck was seen entering the Merlion park on 2 Jan 1:58am. The truck left the park at 3:12am.
+ -The license plate on the truck is GBF 2546 J.
+ -The truck is registered to “Lion Construction Company”.
+
+Lion Construction Company
+ -The company is headquartered in Paya Lebar industrial estate and is a licensed contractor for the merlion maintenance.
+ -The company was requested by STB to conduct an unscheduled maintenance on 2 Jan, with instructions to leave the equipment on-site after maintenance.
+ -Bob was rostered to conduct the merlion maintenance on 2 Jan early morning.
+ -Preliminary investigations show Bob to be an animal lover with the lion as his favourite animal.
+
+Singapore Tourism Board (STB)
+ -STB oversees scheduling the merlion maintenance.
+ -The maintenance schedule is publicly available on the STB website.
+ -John is the manager in charge of scheduling maintenance for tourist attractions.
+ -John instructed Lion Construction Company to leave the equipment at the site after conducting maintenance.
+
+John
+ -John has been working at STB for 5 years.
+ -John is a patron of the arts and is active on social media promoting up-and-coming artist.
+ -Just before the merlion disappearance, there were unusual telephone logs to an unknown number.
+
+Interviews from Members of the Public
+ -Prior to the disappearance, visitors interviewed mentioned that the merlion did not seem securely fastened to the base and would wobble when winds were strong.
+
+Maritime Logs on 2 Jan
+ -There were many boats moored along the Singapore River that night.
+ -The only active boat during the night was Bumboat “X19545B”.
+ -3:20am: Bumboat “X19545B” was in the vicinity of the merlion.
+ -4:10am: Bumboat “X19545B” was in the vicinity of the Keppel Bay.
+
+Barge “X19545B”
+ -20m long vessel belonging to “Boating Forever Club”
+ -Rented by Singapore Art Forever Society from 1 Jan to 3 Jan.
+ -The rental company noted that the boat was returned with 10m of heavy-duty sailing rope left on the deck.
+
+Singapore Art Forever Society
+ -The society was founded in 1995 by 48-year-old Sally
+ -The society owns Warehouse 21 along Keppel Bay
+ -The society is known to attract struggling avant-garde sculptors.
+
+Warehouse 21
+ -Preliminary visits to the warehouse show that it is locked with the windows blacked out. Investigators were unable to view the contents inside.
+ -Interviews from neighbouring warehouses state that heavy machine noises have been heard recently.
+
+Sally
+ -Sally is a pet lover with 5 cats, 3 dogs and a goldfish.
+ -Past posts on IG shows her frustration with the governments lack of funding for the local arts scene.
+ -Recently posted on IG that she will unveiling a surprise sculpture on Chinese New Year eve. The post was liked by John."""
+
+    try:
+        response = openai.ChatCompletion.create(
+            engine="432",
+            messages=[
+                {"role": "system", "content": initial_context},
+                {"role": "assistant", "content": user_input}
+            ], temperature=0.2, top_p=1 
+        )
+        interpreted_query = response['choices'][0]['message']['content']
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+    return jsonify({"response": interpreted_query})
+
 
 
 # Run Server
